e6afe83a0b42e3e75326a5a43051d93fcdacb779
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-07 15:10:18 -0500

    Add a test

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+  });
 });
