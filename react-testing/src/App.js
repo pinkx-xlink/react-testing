d54743db2e93a5bbbe946372a91193f910fa1d49
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-10 19:52:09 -0500

    Delete test from index.test.js and remove unnecessary lines of code

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,7 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-
 const App = () => <h1>Our First Test</h1>;
 
 export default App;
--- react-testing/src/Index.test.js
+++ react-testing/src/Index.test.js
@@ -1,16 +1,4 @@
-import { render, screen } from '@testing-library/react';
 
-import App from './App';
-
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
-  });
-});
 
 // syntax:
 // import { describe, it, expect } from 'vitest';
